06a48f10c38cd4947f592d50e366ce6b4af010aa
hufan <hala9404043@gmail.com>
2020-10-21 11:47:36 +0800

    first commit

---
+++ src/main/java/com/orange/cloudformwork/CloudformworkMasterApplication.java
@@ -0,0 +1,13 @@
+package com.orange.cloudformwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudformworkMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudformworkMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orange/cloudformwork/CloudformworkMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.cloudformwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudformworkMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
